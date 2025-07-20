60cbc01b933152df754e1c399da7b4ff59a2a37e
AyangNovaAnggraeni <ayangnovaanggraeni371@gmail.com>
2025-07-20 16:29:16 +0700

    changing the folder structure

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const backToTopBtn = document.getElementById("backToTop");
-
-window.addEventListener("scroll", () => {
-  backToTopBtn.style.display = window.scrollY > 200 ? "block" : "none";
-});
-
-backToTopBtn.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: 'smooth' });
-});
